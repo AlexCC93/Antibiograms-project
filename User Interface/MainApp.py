904732b13bf78720618724ea0005b52d399fa4db
ficEduIA <40896840+ficEduIA@users.noreply.github.com>
2019-01-23 20:19:57 -0400

    A second glance of main window
    Picture max size:
    750x510

--- User Interface/MainApp.py
+++ User Interface/MainApp.py
@@ -72,52 +72,71 @@ class antibiogApp(wx.Frame):
 		#Main Data Window
 		elif flag_main_window==2:
 			
+			fonte2=wx.Font(11,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_LIGHT)
+			fonte_buttons=wx.Font(16,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_ITALIC,wx.FONTWEIGHT_LIGHT)
 			self.panel_2=wx.Panel(self)
-			box_vert_main_window=wx.BoxSizer(wx.VERTICAL)
-			box_hor_picture_data=wx.BoxSizer(wx.HORIZONTAL)
-			box_vert_data=wx.BoxSizer(wx.VERTICAL)
+			bag_sizer = wx.GridBagSizer(5,5)
+			box_results=wx.StaticBox(self.panel_2,label='Resultados')
+			box_results.SetFont(fonte2)
+			box_vert_data=wx.StaticBoxSizer(box_results,wx.VERTICAL)
 			box_hor_buttons=wx.BoxSizer(wx.HORIZONTAL)
 
-			fonte2=wx.Font(12,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_LIGHT)
-			#Data Display panel
+			
+			#Data Display 
 			m,n,s = np.shape(circles)
 			text=[]
 			static_texts=[]
 			for x in range(n):
-				text.append('centro x: '+str(circles[0,x,0])+'\tcentro y: '+str(circles[0,x,1])+'\tRadio:'+str(circles[0,x,2]))
-				static_texts.append(wx.StaticText(self.panel_2,label=text[x],style=wx.ALIGN_LEFT))
+				text.append('Centro x: '+str(circles[0,x,0])+'	Centro y: '+str(circles[0,x,1])+'	Radio: '+str(circles[0,x,2]))
+				static_texts.append(wx.StaticText(self.panel_2,label=text[x],style=wx.ALIGN_LEFT,size=(340,16)))
 				static_texts[x].SetFont(fonte2)
-				box_vert_data.Add(static_texts[x], flag=wx.ALL,border=20)
+				box_vert_data.Add(static_texts[x], flag=wx.ALL|wx.EXPAND,border=6)
 
 
-			#Picture panel			
+			#Picture 			
 			self.picture = wx.StaticBitmap(self.panel_2,wx.ID_ANY,wx.Bitmap("picture.jpeg"))
-			box_hor_picture_data.Add(self.picture,flag=wx.RIGHT|wx.TOP|wx.BOTTOM|wx.EXPAND|wx.ALIGN_LEFT,border=15)
+		
 
 
-			#Buttons Panel
-			button_new_picture=wx.Button(self.panel_2,label='Nueva Foto')
-			button_edit=wx.Button(self.panel_2,label='Editar')
-			button_save=wx.Button(self.panel_2,label='Guardar')
-			button_back=wx.Button(self.panel_2,label='Atras')
+			#Buttons 
+			button_new_picture=wx.Button(self.panel_2,label='Nueva Foto',size=(180,60))
+			button_edit=wx.Button(self.panel_2,label='Editar',size=(180,60))
+			button_save=wx.Button(self.panel_2,label='Guardar',size=(180,60))
+			button_back=wx.Button(self.panel_2,label='Atras',size=(180,60))
+
+			button_new_picture.SetFont(fonte_buttons)
+			button_edit.SetFont(fonte_buttons)
+			button_save.SetFont(fonte_buttons)
+			button_back.SetFont(fonte_buttons)
 
 			# button_new_picture.Bind(wx.EVT_BUTTON, self.onTakeNewPicture)
 			# button_edit.Bind(wx.EVT_BUTTON, onEditPicture)
 			# button_save.Bind(wx.EVT_BUTTON, self.onSaveData)
 			# button_back.Bind(wx.EVT_BUTTON, onBackToMenu)
 
-			box_hor_buttons.Add(button_new_picture, flag=wx.EXPAND|wx.ALL,border=25)
-			box_hor_buttons.Add(button_edit, flag=wx.EXPAND|wx.ALL,border=25)
-			box_hor_buttons.Add(button_save, flag=wx.EXPAND|wx.ALL,border=25)
-			box_hor_buttons.Add(button_back, flag=wx.EXPAND|wx.ALL,border=25)
+			box_hor_buttons.Add(button_new_picture, flag=wx.EXPAND|wx.LEFT|wx.RIGHT,border=20)
+			box_hor_buttons.Add(button_edit, flag=wx.EXPAND|wx.LEFT|wx.RIGHT,border=20)
+			box_hor_buttons.Add(button_save, flag=wx.EXPAND|wx.LEFT|wx.RIGHT,border=20)
+			box_hor_buttons.Add(button_back, flag=wx.EXPAND|wx.LEFT|wx.RIGHT,border=20)
 
-			#Ordering Panels and BoxSizers
-			box_hor_picture_data.Add(box_vert_data,flag=wx.ALIGN_RIGHT,border=2)
-			box_vert_main_window.Add(box_hor_picture_data,flag=wx.CENTER,border=5)
-			box_vert_main_window.Add(box_hor_buttons,flag=wx.CENTER,border=5)
+			#Setting separators
+			line_hor=wx.StaticLine(self.panel_2)
+
+			#Ordering GridBack and BoxSizers
+			bag_sizer.Add(self.picture,pos=(0,0),span=(3,3),flag=wx.EXPAND|wx.ALIGN_LEFT|wx.TOP|wx.RIGHT|wx.BOTTOM,border=20)
+			bag_sizer.Add(line_hor, pos=(3,0),span=(1,4),flag=wx.EXPAND|wx.BOTTOM,border=0)
+			bag_sizer.Add(box_vert_data,pos=(0,3),span=(3,1),flag=wx.ALIGN_RIGHT|wx.ALL|wx.EXPAND,border=25)
+			bag_sizer.Add(box_hor_buttons,pos=(4,1),span=(1,3),flag=wx.ALIGN_BOTTOM|wx.ALIGN_RIGHT|wx.ALL,border=60)
 			
-			self.panel_2.SetSizer(box_vert_main_window)
 
+			bag_sizer.AddGrowableCol(0)
+			bag_sizer.AddGrowableRow(2)
+
+			self.panel_2.SetSizer(bag_sizer)
+			bag_sizer.Fit(self)
+
+			self.SetSize(1500,800)
+			self.SetSizeHints(300,500,1600,850)
 
 		#Info. Window
 		elif flag_main_window==3:
@@ -159,7 +178,7 @@ app = wx.App()
 wind_app = antibiogApp(None)
 app.MainLoop()
 #Radious and centers of circles example picture 2.jpeg
-circles=np.array([[[246, 134, 39],[48, 116, 39],[186, 214, 39],[218, 62, 40]]])
+circles=np.array([[[236, 194,  40],[248, 312,  40],[330, 244,  40], [158, 438,  40],[370, 432,  40],[242,  58,  40],[ 96, 410,  40],[406, 432,  40]]])
 
 def main():
 
